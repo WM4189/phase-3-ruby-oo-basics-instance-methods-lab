9c9e6477ea09b5fe47cbbf65d3adabb0cbdd9465
William Mosto <will.mosto@gmail.com>
2021-09-21 08:11:07 -0400

    commit tree 75b0688942562aeadd9109b74acf453ff5d708b3

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+    def talk
+        puts "Hello World!" 
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+tyler_durden = Person.new
+tyler_durden.talk
+tyler_durden.walk
+
+# ruby lib/person.rb
